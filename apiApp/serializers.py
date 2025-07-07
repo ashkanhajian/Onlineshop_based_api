a654d85f62d8fba179db4fa0fb3439476d6aec0d
nell2008 <ashkan.spg@gmail.com>
2025-07-07 12:27:51 +0330

    Initial commit

--- apiApp/serializers.py
+++ apiApp/serializers.py
@@ -2,13 +2,23 @@ from rest_framework import serializers
 from apiApp.models import Product, Category
 
 
-class ProductSerializer(serializers.ModelSerializer):
+class ProductListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['id','name','slug','description','price']
+        fields = ['id','name','slug','price']
+class ProductDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id','name','slug','price','description']
 
-class CategorySerializer(serializers.ModelSerializer):
-    products = ProductSerializer(many=True, read_only=True)
+class CategoryDetailSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+    class Meta:
+        model = Category
+        fields = ['id','name','image']
+
+class CategoryListSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
     class Meta:
         model = Category
         fields = ['id','name','image']
--- apiApp/views.py
+++ apiApp/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from apiApp.models import Product
-from apiApp.serializers import ProductSerializer
+from apiApp.models import Product, Category
+from apiApp.serializers import ProductListSerializer, ProductDetailSerializer, CategoryListSerializer, CategoryDetailSerializer
 
 
 # Create your views here.
 @api_view(['GET'])
 def product_list(request):
     products = Product.objects.filter(featured=True)
-    serializer = ProductSerializer(products, many=True)
+    serializer = ProductListSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, slug):
+    product = Product.objects.get(slug=slug)
+    serializer = ProductDetailSerializer(product)
+    return Response(serializer.data)
+
+@api_view(['Get'])
+def category_list(request):
+    categories = Category.objects.all()
+    serializer = CategoryListSerializer(categories, many=True)
+    return Response(serializer.data)
+@api_view(['Get'])
+def category_detail(request, slug):
+    category = Category.objects.get(slug=slug)
+    serializer = CategoryDetailSerializer(category)
     return Response(serializer.data)
