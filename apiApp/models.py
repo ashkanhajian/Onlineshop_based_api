09703457580cdd2b32167296ddea493af8b8719c
nell2008 <ashkan.spg@gmail.com>
2025-07-08 08:29:59 +0330

    Initial commit

--- apiApp/models.py
+++ apiApp/models.py
@@ -35,3 +35,17 @@ class Product(models.Model):
                 count += 1
             self.slug = unique_slug_generator
         super().save(*args, **kwargs)
+
+class Cart(models.Model):
+    cart_code= models.CharField(max_length=11, unique=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.cart_code
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cartitems')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='item')
+    quantity = models.PositiveIntegerField(default=1)
+    def __str__(self):
+        return f"{self.quantity} x {self.product.name} in cart {self.cart.cart_code}"
--- apiApp/serializers.py
+++ apiApp/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from apiApp.models import Product, Category
+from apiApp.models import Product, Category, CartItem, Cart
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -22,3 +22,34 @@ class CategoryDetailSerializer(serializers.ModelSerializer):
         model = Category
         fields = ['id','name','image']
 
+class CartItemListSerializer(serializers.ModelSerializer):
+    products = ProductListSerializer(many=True, read_only=True)
+    sub_total = serializers.SerializerMethodField()
+    class Meta:
+        model = CartItem
+        fields = ['id', 'product', 'quantity','sub_total']
+
+    def get_sub_total(self, cartitem):
+        total = cartitem.product.price * cartitem.quantity
+        return total
+
+class CartSerializer(serializers.ModelSerializer):
+    cart_items = CartItemListSerializer(many=True, read_only=True)
+    cart_total = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart
+        fields = ['id', 'cart_items','cart_code']
+    def get_cart_items(self, cart):
+        items = cart.cart_items.all()
+        total = sum([items.quantity * items.price for items in items])
+        return total
+
+class CartStatusSerializer(serializers.ModelSerializer):
+    total_quantity = serializers.SerializerMethodField()
+    class Meta:
+        model = Cart
+        fields = ['id','cart_code', 'total_quantity']
+    def get_total_quantity(self, cart):
+        items = cart.cart_items.all()
+        total = sum([items.quantity * items.price for items in items])
+        return total
