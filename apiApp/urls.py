4cb0cfc471bb8c34cd4871dfa638efd05857e68c
nell2008 <ashkan.spg@gmail.com>
2025-07-17 13:08:25 +0330

    Initial commit

--- apiApp/urls.py
+++ apiApp/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('add_review/', views.add_review, name='add_review'),
     path('update_review/<int:pk>/', views.update_review, name='update_review'),
     path('delete_review/<int:pk>/', views.delete_review, name='delete_review'),
-    path("add_to_wishlist/",views.add_to_whishlist, name="add_to_wishlist"),
     path('delete_cartitem/<int:pk>/', views.delete_cartitem, name='delete_cartitem'),
+    path("add_to_wishlist/",views.add_to_whishlist, name="add_to_wishlist"),
+
 ]
